ad6fca88349547cfe8b01bbae1afbfc38980f773
Deshaun Breeden <thisisdeshaun@gmail.com>
2022-11-30 22:26:01 -0500

    Edit RandomPasswordGenerator.java

--- RandomPasswordGenerator.java
+++ RandomPasswordGenerator.java
@@ -4,6 +4,7 @@ public class RandomPasswordGenerator {
 
     static Scanner input = new Scanner(System.in);
     static String name;
+    static String menuChoice;
 
     public static void UserName() {
         System.out.print("Please enter your name: ");
@@ -11,21 +12,68 @@ public class RandomPasswordGenerator {
         System.out.println("\nHello " + name + "!");
     }
 
+    public static void menuOptions(){
+        int menuChoice = 0;
+        String menuOption;
+
+        System.out.println("1. Generate a number password");
+        System.out.println("2. Generate a lowercase password");
+        System.out.println("3. Generate a uppercase password");
+        System.out.println("4. Generate a lower & upper case password");
+        System.out.println("5. Generate a full strength password (Upper, Lower, and Number)");
+
+        switch (menuChoice){
+            case 1: 
+            menuOption = "\nGenerate a number password";
+            break;
+            case 2:
+            menuOption = "Generate a lowercase password";
+            break;
+            case 3:
+            menuOption = "Generate a uppercase password";
+            break;
+            case 4:
+            menuOption = "Generate a lower & upper case password";
+            break;
+            case 5:
+            menuOption = "Generate a full strength password (Upper, Lower, and Number)";
+            break;
+        }
+        System.out.print("\nWhat password would you like? ");
+        menuOption = input.next();
+    }
+
+    public static int passLength(int passwordLength) {
+        int userpasswordLength;
+        do {
+        userpasswordLength = input.nextInt();
+        System.out.println("\nHow long would you like your password to be (Must be between 1-16) ");
+        } while (userpasswordLength >= 1 || userpasswordLength <= 16);
+        return userpasswordLength;
+    }
+
     public static void main(String args[]) {
         UserName();
 
         System.out.println("This is a random password generator.");
         System.out.println("You will be given a menu of options to choose from in order to generate your desired password.");
+        System.out.println("You will be given 5 options to choose from.");
 
-        System.out.print("\nAre you ready to begin?(Type Y to continue) ");
+
+        System.out.print("\nAre you ready to begin?(Type Y to continue or N to exit) ");
         String begin = input.next();
 
         if (begin.equals("Y")){
             System.out.println("\nPlease choose from the menu provided below:");
-        }else {
-            System.out.println("Thank you for using my program!");
+        }else if (begin.equals("N")) {
+            System.out.println("\nThank you for using my program!");
         }
 
-        System.out.println("\n");
+        menuOptions();
+        
+        int passwordLength = 0;
+        passLength(passwordLength);
+        
+        
     }
 }
