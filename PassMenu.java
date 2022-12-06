ed0c6f035fddf3074f78a0ebc12e158d8085aaf3
Deshaun Breeden <thisisdeshaun@gmail.com>
2022-12-05 19:52:05 -0500

    Edit RandomPasswordGenerator.java and PassMenu.java

---
+++ PassMenu.java
@@ -0,0 +1,11 @@
+public class PassMenu {
+    public void printMenu(){
+        System.out.println("\nPlease Choose from the menu provided below:");
+        System.out.println("\n1. Generate a number password");
+        System.out.println("2. Generate a lowercase password");
+        System.out.println("3. Generate a uppercase password");
+        System.out.println("4. Generate a lower & upper case password");
+        System.out.println("5. Generate a full strength password (Upper, Lower, and Number)");
+        System.out.println("0. Exit");
+    }
+}
