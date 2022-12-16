80bae8f911d8cbb1b6309bc9453d2ed7ad456be3
Deshaun Breeden <thisisdeshaun@gmail.com>
2022-12-16 16:10:04 -0500

    Added a default close file for the default switch statement, finished the full strength method, corrected user input errors, created new loops to offer user more changes to correct input

---
+++ defaultClose.java
@@ -0,0 +1,7 @@
+public class defaultClose {
+    public void dClose() {
+System.out.println("You entered an unknown response.");
+System.out.println("Please re-run the program.");
+System.exit(0);
+}
+}
